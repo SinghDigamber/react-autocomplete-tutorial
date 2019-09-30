f5746ee5544ef1d5256bc3d916326213770a01d0
SinghDigamber <digambersingh126@gmail.com>
2019-09-30 22:50:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { MoviesData, renderMovieTitle } from './movies-data';
+import Autocomplete from 'react-autocomplete';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = { val: '' };
+
+  render() {
+    return (
+      <div className="autocomplete-wrapper">
+        <h3>React Autocomplete Demo</h3>
+        <Autocomplete
+          value={this.state.val}
+          items={MoviesData()}
+          getItemValue={item => item.title}
+          shouldItemRender={renderMovieTitle}
+          renderMenu={item => (
+            <div className="dropdown">
+              {item}
+            </div>
+          )}
+          renderItem={(item, isHighlighted) =>
+            <div className={`item ${isHighlighted ? 'selected-item' : ''}`}>
+              {item.title}
+            </div>
+          }
+          onChange={(event, val) => this.setState({ val })}
+          onSelect={val => this.setState({ val })}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
